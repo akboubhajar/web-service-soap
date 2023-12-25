408fcd9239865edc34df4d9a1852501b03b169c2
akboubhajar <hajarakboub@gmail.com>
2023-12-25 12:42:01 +0100

    first commit

---
+++ src/main/java/ws/ServerJWS.java
@@ -0,0 +1,10 @@
+package ws;
+import jakarta.xml.ws.Endpoint;
+public class ServerJWS {
+    public static void main(String[] args){
+        String url="http://0.0.0.0:8787/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("Web Service deploye sur"+url);
+    }
+
+}
